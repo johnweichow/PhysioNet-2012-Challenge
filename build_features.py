a851c4d1f682af62178a5b002753f64f748bfb8e
johnweichow <johnweichow@gmail.com>
2020-01-26 15:02:52 -0500

    added contest hold-out data, updated modeling_approach writeup

--- build_features.py
+++ build_features.py
@@ -7,123 +7,129 @@ import os
 import math
 from tsfresh.feature_extraction import extract_features
 
-# import data
-filepath = os.path.join('.', 'data')
-df_ts = pd.read_csv(os.path.join(filepath, 'interim', 'set-a_timeseries.csv'))
-df_gen_desc = pd.read_csv(os.path.join(filepath, 'interim', 'set-a_general-descriptors.csv'))
-df_outcomes = pd.read_csv(os.path.join(filepath, 'raw', 'Outcomes-a.txt'))
-
-# pivot general descriptors so each row is one ID
-df_gen_desc = (
-	pd.pivot_table(df_gen_desc, values='Value', index='RecordID', columns='Parameter')
-	  .reset_index()
-)
-
-# one-hot encode ICUType
-df_gen_desc.loc[:, 'ICUType'] = df_gen_desc.loc[:, 'ICUType'].apply(int)
 df_ICU_onehot = pd.get_dummies(df_gen_desc['ICUType'], prefix='ICUType', drop_first=True)
-df_gen_desc.drop('ICUType', axis=1, inplace=True)
-df_gen_desc = pd.merge(df_gen_desc, df_ICU_onehot, left_index=True, right_index=True, how='left')
+def build_features(set_name):
+	# import data
+	filepath = os.path.join('.', 'data')
+	df_ts = pd.read_csv(os.path.join(filepath, 'interim', 'set-'+set_name+'_timeseries.csv'))
+	df_gen_desc = pd.read_csv(os.path.join(filepath, 'interim', 'set-'+set_name+'_general-descriptors.csv'))
+	df_outcomes = pd.read_csv(os.path.join(filepath, 'raw', 'Outcomes-'+set_name+'.txt'))
 
-# create list of features to calculate for each patient parameter. 
-default_fc_params_custom = {
-	'median': None,
-    'variance': None,
-    'maximum': None,
-    'minimum': None,
-    'length': None, #denotes count
-    'linear_trend': [{'attr': 'slope'}]
-}
+	# pivot general descriptors so each row is one ID
+	df_gen_desc = (
+		pd.pivot_table(df_gen_desc, values='Value', index='RecordID', columns='Parameter')
+			.reset_index()
+	)
 
-# Create features that count the occurences of parameters in certain ranges. 
-# Approach is based on SAPS-II scoring system, where a patient's risk score 
-# increases for values that are too low or high
-kind_to_fc_params_custom = {
-    'HR': {'range_count': [{'min': 120, 'max': 9999},
-                           {'min': 70, 'max': 119},
-                           {'min': 40, 'max': 69},
-                           {'min': 0, 'max': 39}]},
-    'SysABP': {'range_count': [{'min': 200, 'max': 9999},
-                           {'min': 100, 'max': 199},
-                           {'min': 70, 'max': 99},
-                           {'min': 0, 'max': 69}]},
-    'Na': {'range_count': [{'min': 145, 'max': 9999},
-                           {'min': 125, 'max': 144},
-                           {'min': 0, 'max': 124}]},
-    'K': {'range_count': [{'min': 5.0, 'max': 9999},
-                           {'min': 3.0, 'max': 4.9},
-                           {'min': 0, 'max': 3.0}]},
-    'WBC': {'range_count': [{'min': 20.1, 'max': 9999},
-                           {'min': 1.01, 'max': 20},
-                           {'min': 0, 'max': 1.0}]},
-}
+	# one-hot encode ICUType
+	df_gen_desc.loc[:, 'ICUType'] = df_gen_desc.loc[:, 'ICUType'].apply(int)
+	df_ICU_onehot = pd.get_dummies(df_gen_desc['ICUType'], prefix='ICUType', drop_first=True)
+	df_gen_desc.drop('ICUType', axis=1, inplace=True)
+	df_gen_desc = pd.merge(df_gen_desc, df_ICU_onehot, left_index=True, right_index=True, how='left')
 
-df_ts_tsfresh = extract_features(df_ts.dropna().set_index('Time'), column_id = 'RecordID',
-                                  column_kind = 'Parameter', column_value='Value',
-                                  default_fc_parameters=default_fc_params_custom,
-                                  kind_to_fc_parameters=kind_to_fc_params_custom)
+	# create list of features to calculate for each patient parameter. 
+	default_fc_params_custom = {
+		'median': None,
+			'variance': None,
+			'maximum': None,
+			'minimum': None,
+			'length': None, #denotes count
+			'linear_trend': [{'attr': 'slope'}]
+	}
 
-df_ts_tsfresh = df_ts_tsfresh.reset_index().rename(columns={'id':'RecordID'})
+	# Create features that count the occurences of parameters in certain ranges. 
+	# Approach is based on SAPS-II scoring system, where a patient's risk score 
+	# increases for values that are too low or high
+	kind_to_fc_params_custom = {
+			'HR': {'range_count': [{'min': 120, 'max': 9999},
+														{'min': 70, 'max': 119},
+														{'min': 40, 'max': 69},
+														{'min': 0, 'max': 39}]},
+			'SysABP': {'range_count': [{'min': 200, 'max': 9999},
+														{'min': 100, 'max': 199},
+														{'min': 70, 'max': 99},
+														{'min': 0, 'max': 69}]},
+			'Na': {'range_count': [{'min': 145, 'max': 9999},
+														{'min': 125, 'max': 144},
+														{'min': 0, 'max': 124}]},
+			'K': {'range_count': [{'min': 5.0, 'max': 9999},
+														{'min': 3.0, 'max': 4.9},
+														{'min': 0, 'max': 3.0}]},
+			'WBC': {'range_count': [{'min': 20.1, 'max': 9999},
+														{'min': 1.01, 'max': 20},
+														{'min': 0, 'max': 1.0}]},
+	}
 
-# for features that measure the count of a parameter, impute nulls with 0
-for colname in df_ts_tsfresh.columns:
-	if '__length' in colname or 'range_count' in colname:
-		df_ts_tsfresh[colname].fillna(0, inplace=True)
+	df_ts_tsfresh = extract_features(df_ts.dropna().set_index('Time'), column_id = 'RecordID',
+																		column_kind = 'Parameter', column_value='Value',
+																		default_fc_parameters=default_fc_params_custom,
+																		kind_to_fc_parameters=kind_to_fc_params_custom)
 
-# define helper functions to handle dataframes from custom timeseries calculations
-def drop_and_flatten(df):
-    df.columns = df.columns.droplevel(0) #drops unwanted 'Value level'
-    df.columns = ['_'.join(col).rstrip('_') for col in df.columns.values] #flatten columns
-    df.reset_index(inplace=True)
-    return df
+	df_ts_tsfresh = df_ts_tsfresh.reset_index().rename(columns={'id':'RecordID'})
 
-# get first and last values in timeseries for each patient parameter
-df_ts_first_last = (
-	df_ts.loc[:, ['RecordID', 'Parameter', 'Value']]
-         .groupby(['RecordID', 'Parameter'])
-         .agg(['first', 'last'])
-         .unstack()
-)
+	# for features that measure the count of a parameter, impute nulls with 0
+	for colname in df_ts_tsfresh.columns:
+		if '__length' in colname or 'range_count' in colname:
+			df_ts_tsfresh[colname].fillna(0, inplace=True)
 
-df_ts_first_last = df_ts_first_last.swaplevel(axis=1)
-df_ts_first_last = drop_and_flatten(df_ts_first_last)
+	# define helper functions to handle dataframes from custom timeseries calculations
+	def drop_and_flatten(df):
+			df.columns = df.columns.droplevel(0) #drops unwanted 'Value level'
+			df.columns = ['_'.join(col).rstrip('_') for col in df.columns.values] #flatten columns
+			df.reset_index(inplace=True)
+			return df
 
-# get mean value of each patient parameter over each 6 hour period
-TIME_PERIOD = 6
-df_ts['Time_group'] = df_ts['Time'].apply(lambda x:
-	'hrs-' 
-	+ str(math.trunc((x-1)/(TIME_PERIOD * 60))*TIME_PERIOD + 1)
-	+ '-'
-	+ str((math.trunc((x-1)/(TIME_PERIOD * 60))+1)*TIME_PERIOD)
-)
+	# get first and last values in timeseries for each patient parameter
+	df_ts_first_last = (
+		df_ts.loc[:, ['RecordID', 'Parameter', 'Value']]
+					.groupby(['RecordID', 'Parameter'])
+					.agg(['first', 'last'])
+					.unstack()
+	)
 
-df_ts_means = (
-	df_ts.loc[:, ['RecordID', 'Parameter', 'Time_group', 'Value']]
-		 .groupby(['RecordID', 'Parameter', 'Time_group'])
-		 .agg(['mean'])
-		 .unstack()
-		 .unstack()
-)
+	df_ts_first_last = df_ts_first_last.swaplevel(axis=1)
+	df_ts_first_last = drop_and_flatten(df_ts_first_last)
 
-df_ts_means = df_ts_means.swaplevel(i=-2, j=-1, axis=1)
-df_ts_means = df_ts_means.swaplevel(i=-3, j=-2, axis=1)
-df_ts_means = drop_and_flatten(df_ts_means)
+	# get mean value of each patient parameter over each 6 hour period
+	TIME_PERIOD = 6
+	df_ts['Time_group'] = df_ts['Time'].apply(lambda x:
+		'hrs-' 
+		+ str(math.trunc((x-1)/(TIME_PERIOD * 60))*TIME_PERIOD + 1)
+		+ '-'
+		+ str((math.trunc((x-1)/(TIME_PERIOD * 60))+1)*TIME_PERIOD)
+	)
 
-# merge timeseries features with general descriptors into one dataframe
-df_features = (
-	pd.merge(df_gen_desc, df_ts_tsfresh, how='left',on='RecordID', validate='1:1')
-	  .merge(df_ts_first_last, how='left', on='RecordID', validate='1:1')
-	  .merge(df_ts_means, how='left', on='RecordID', validate='1:1')
-)
+	df_ts_means = (
+		df_ts.loc[:, ['RecordID', 'Parameter', 'Time_group', 'Value']]
+			.groupby(['RecordID', 'Parameter', 'Time_group'])
+			.agg(['mean'])
+			.unstack()
+			.unstack()
+	)
 
-# merge features with outcomes
-df_features_outcomes = (
-	pd.merge(df_features, df_outcomes.loc[:, ['RecordID', 'In-hospital_death']],
-	         how='left', on='RecordID', validate='1:1')
-)
+	df_ts_means = df_ts_means.swaplevel(i=-2, j=-1, axis=1)
+	df_ts_means = df_ts_means.swaplevel(i=-3, j=-2, axis=1)
+	df_ts_means = drop_and_flatten(df_ts_means)
 
-# save processed data to disk
-export_folder = os.path.join('.', 'data', 'processed')
-os.makedirs(export_folder)
-filepath = os.path.join(export_folder, 'features_and_outcomes.csv')
-df_features_outcomes.to_csv(filepath, index=False)
+	# merge timeseries features with general descriptors into one dataframe
+	df_features = (
+		pd.merge(df_gen_desc, df_ts_tsfresh, how='left',on='RecordID', validate='1:1')
+			.merge(df_ts_first_last, how='left', on='RecordID', validate='1:1')
+			.merge(df_ts_means, how='left', on='RecordID', validate='1:1')
+	)
+
+	# merge features with outcomes
+	df_features_outcomes = (
+		pd.merge(df_features, df_outcomes.loc[:, ['RecordID', 'In-hospital_death']],
+						how='left', on='RecordID', validate='1:1')
+	)
+
+	# save processed data to disk
+	export_folder = os.path.join('.', 'data', 'processed')
+	if not os.path.isdir(export_folder):
+		os.makedirs(export_folder)
+	filepath = os.path.join(export_folder, 'features_and_outcomes-'+set_name+'.csv')
+	df_features_outcomes.to_csv(filepath, index=False)
+
+build_features('a')
+build_features('c')
--- make_dataset.py
+++ make_dataset.py
@@ -6,65 +6,76 @@ import numpy as np
 import os
 import wget
 import zipfile
+import tarfile
 
 # save raw data to disk and unzip data
 filepath = os.path.join('.', 'data', 'raw')
 os.makedirs(filepath)
 wget.download('https://physionet.org/files/challenge-2012/1.0.0/set-a.zip', filepath)
 wget.download('https://physionet.org/files/challenge-2012/1.0.0/Outcomes-a.txt', filepath)
+wget.download('https://physionet.org/files/challenge-2012/1.0.0/Outcomes-c.txt', filepath)
+wget.download('https://physionet.org/files/challenge-2012/1.0.0/set-c.tar.gz', filepath)
 
 with zipfile.ZipFile(os.path.join(filepath, 'set-a.zip'), 'r') as zip_ref:
     zip_ref.extractall(filepath)
 
-# join patient files into one dataframe
-df = pd.DataFrame()
-fp = os.path.join('.', 'data', 'raw', 'set-a')
-for filename in os.listdir(fp):
-    with open(os.path.join(fp, filename), 'r') as openfile:
-        df_temp = pd.read_csv(openfile)
-        df_temp['RecordID'] = df_temp[df_temp['Parameter']=='RecordID'].at[0,'Value'].astype(int) # create RecordID column
-        df = pd.concat([df, df_temp], ignore_index=True)
-# set negative values to null
-df.loc[df['Value']<0, 'Value'] = np.nan
+with tarfile.open(os.path.join(filepath, 'set-c.tar.gz'), 'r:gz') as tar_ref:
+    tar_ref.extractall(filepath)
 
-# remove outliers and impossible values
-df.loc[(df['Parameter']=='pH') & (df['Value']<6.8), 'Value'] = np.nan
-df.loc[(df['Parameter']=='pH') & (df['Value']>8), 'Value'] = np.nan
+def make_interim_data(set_name):
+    # join patient files into one dataframe
+    df = pd.DataFrame()
+    fp = os.path.join('.', 'data', 'raw', set_name)
+    for filename in os.listdir(fp):
+        with open(os.path.join(fp, filename), 'r') as openfile:
+            df_temp = pd.read_csv(openfile)
+            df_temp['RecordID'] = df_temp[df_temp['Parameter']=='RecordID'].at[0,'Value'].astype(int) # create RecordID column
+            df = pd.concat([df, df_temp], ignore_index=True)
+    # set negative values to null
+    df.loc[df['Value']<0, 'Value'] = np.nan
 
-df.loc[(df['Parameter']=='Temp') & (df['Value']<30), 'Value'] = np.nan
-df.loc[(df['Parameter']=='Temp') & (df['Value']>45), 'Value'] = np.nan
+    # remove outliers and impossible values
+    df.loc[(df['Parameter']=='pH') & (df['Value']<6.8), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='pH') & (df['Value']>8), 'Value'] = np.nan
 
-df.loc[(df['Parameter']=='HR') & (df['Value']<1), 'Value'] = np.nan
-df.loc[(df['Parameter']=='HR') & (df['Value']>250), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='Temp') & (df['Value']<30), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='Temp') & (df['Value']>45), 'Value'] = np.nan
 
-df.loc[(df['Parameter']=='RespRate') & (df['Value']<1), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='HR') & (df['Value']<1), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='HR') & (df['Value']>250), 'Value'] = np.nan
 
-df.loc[(df['Parameter']=='Weight') & (df['Value']<20), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='RespRate') & (df['Value']<1), 'Value'] = np.nan
 
-df.loc[(df['Parameter']=='SysABP') & (df['Value']<1), 'Value'] = np.nan
-df.loc[(df['Parameter']=='NISysABP') & (df['Value']<1), 'Value'] = np.nan
-df.loc[(df['Parameter']=='MAP') & (df['Value']<1), 'Value'] = np.nan
-df.loc[(df['Parameter']=='NIMAP') & (df['Value']<1), 'Value'] = np.nan
-df.loc[(df['Parameter']=='DiasABP') & (df['Value']<1), 'Value'] = np.nan
-df.loc[(df['Parameter']=='NIDiasABP') & (df['Value']<1), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='Weight') & (df['Value']<20), 'Value'] = np.nan
 
-# create a separate dataframe w/ general descriptors
-df_gen_desc = df.loc[df['Time']=='00:00', :].copy()
-df_gen_desc = df_gen_desc.loc[df['Parameter'].isin(['Weight', 'Gender', 'Height', 'ICUType', 'Age'])]
+    df.loc[(df['Parameter']=='SysABP') & (df['Value']<1), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='NISysABP') & (df['Value']<1), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='MAP') & (df['Value']<1), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='NIMAP') & (df['Value']<1), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='DiasABP') & (df['Value']<1), 'Value'] = np.nan
+    df.loc[(df['Parameter']=='NIDiasABP') & (df['Value']<1), 'Value'] = np.nan
 
-# Get only the last value per parameter per patient, as some patients have multiple values for Age
-df_gen_desc = df_gen_desc.groupby(['RecordID', 'Parameter'])[['Value']].last().reset_index()
+    # create a separate dataframe w/ general descriptors
+    df_gen_desc = df.loc[df['Time']=='00:00', :].copy()
+    df_gen_desc = df_gen_desc.loc[df['Parameter'].isin(['Weight', 'Gender', 'Height', 'ICUType', 'Age'])]
 
-# drop general descriptors from original dataframe, except for weight, which is recorded as a timeseries
-df = df[~df['Parameter'].isin(['RecordID', 'Gender', 'Height', 'ICUType', 'Age'])]
+    # Get only the last value per parameter per patient, as some patients have multiple values for Age
+    df_gen_desc = df_gen_desc.groupby(['RecordID', 'Parameter'])[['Value']].last().reset_index()
 
-# convert time from string to minutes elapsed
-df['Time'] = df['Time'].map(lambda x: int(x.split(':')[0])*60 + int(x.split(':')[1]))
+    # drop general descriptors from original dataframe, except for weight, which is recorded as a timeseries
+    df = df[~df['Parameter'].isin(['RecordID', 'Gender', 'Height', 'ICUType', 'Age'])]
 
-# save interim data to disk
-filepath = os.path.join('.', 'data', 'interim')
-os.makedirs(filepath)
-os.chdir(filepath)
-df.to_csv('set-a_timeseries.csv', index=False)
-df_gen_desc.to_csv('set-a_general-descriptors.csv', index=False)
-os.chdir(os.path.join(os.pardir, os.pardir))
+    # convert time from string to minutes elapsed
+    df['Time'] = df['Time'].map(lambda x: int(x.split(':')[0])*60 + int(x.split(':')[1]))
+
+    # save interim data to disk
+    filepath = os.path.join('.', 'data', 'interim')
+    if not os.path.isdir(filepath):
+        os.makedirs(filepath)
+    os.chdir(filepath)
+    df.to_csv(set_name+'_timeseries.csv', index=False)
+    df_gen_desc.to_csv(set_name+'_general-descriptors.csv', index=False)
+    os.chdir(os.path.join(os.pardir, os.pardir))
+
+make_interim_data(set_name='set-a')
+make_interim_data(set_name='set-c')
